22f7be66aa757fb570fcf0529166e1f8ac372192
SpaceBasic-Dev <shrijeetdeshmukh@gmail.com>
2023-03-28 22:41:50 +0530

    Updated app.js
    Updated app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import logo from './logo.svg';
-import './CheckoutForm.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import CheckoutFormUpdated from './CheckoutFormUpdated';
+import CheckoutFormUpdated from './screens/checkout/CheckoutFormUpdated';
+import ListOfProducts from './screens/productPage/ListOfProducts';
+import './screens/checkout/CheckoutForm.css';
 
 function App() {
   return (
-    <div >
-      <CheckoutFormUpdated />
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ListOfProducts />} />
+          <Route path="/checkout" element={<CheckoutFormUpdated />} />
+        </Routes>
+      </BrowserRouter>
   );
 }
 
